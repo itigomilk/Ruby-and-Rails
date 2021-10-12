9f0b78aac4eb8f21fe74bd17b6c0018de0bace2e
Hiroki Sato <longuinness_lance@yahoo.co.jp>
2021-10-13 02:41:42 +0900

    css updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :blogs
-  root 'blogs#index'
-  get 'index2'=>'blogs#index2'
+  root "blogs#index"
+  get "index2"=>"blogs#index2"
+  get "content"=>"blogs#content"
 end
