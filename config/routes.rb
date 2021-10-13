65630099947cb5f7bafe94f45bd5989e256bb892
Hiroki Sato <longuinness_lance@yahoo.co.jp>
2021-10-13 16:22:52 +0900

    css updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :blogs
   root "blogs#index"
+  get "review"=>"blogs#review"
+  get "app"=>"blogs#app"
   get "blog"=>"blogs#blog"
   get "content"=>"blogs#content"
 end
